c1cabdc7b70d7efefc30fef6b161ffd8e238e926
morty-9 <hzawnineseven@gmail.com>
2024-09-17 16:56:28 +0630

    landing page

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,4 @@
-class App{
-
+class App {
   constructor() {
     const { innerheight } = window;
     this.innerHeight = innerheight;
@@ -14,88 +13,77 @@ class App{
   }
 
   _setInitialStates() {
-    gsap.set('.hero-bg' ,{
+    gsap.set(".hero-text-p", {
       opacity: 0,
-      y:-30
-   })
+      y: 20,
+    });
   }
+
   _createHeroTextAnimation() {
-    let lastScrollTop = 0;
-  
-    gsap.timeline({
+    
+    const tl = gsap.timeline({
       scrollTrigger: {
         trigger: ".hero",
         pin: true,
-        end: `+=${innerHeight * 0.5}`,
+        end: `+=${innerHeight * 1.5}`,
         scrub: true,
-        onUpdate: () => {
-          const isScrollingDown = scroll > lastScrollTop;
-          document.querySelector('.hero-text span').classList.toggle('red', isScrollingDown);
-          document.querySelector('.hero-text span').classList.toggle('black', !isScrollingDown);
-          lastScrollTop = scroll;
-        }
+        markers: true,
+      
+        
+      },
+    });
+    const spanToBold = document.querySelectorAll(".hero-text-p span");
+    tl.fromTo(
+      ".hero-text",
+      {
+        scale: 2,
+        x: -400,
+        duration: 3,
+        ease: "power3.in",
+        color: "white",
+        "-webkit-text-stroke-width": "1px",
+        "-webkit-text-stroke-color": "black",
+      },
+      {
+        color: "black",
+        scale: 1,
+        x: 0,
       }
-    })
-    .from('.hero-text', {
-      scale: 4,
-      duration: 1.5,
-      ease: "power3.in",
-      x:-200
-    })
-    .to('.hero-bg', {
-      opacity: 1,
-      duration: 1.5,
-      y: 0,
-      ease: "power3.in",
-    }, "1");
-  }
-  
+    );
 
-//  _createHeroTextAnimation() {
-//    let lastScrollTop = 0;
-//
-//    gsap.timeline({
-//      scrollTrigger: {
-//        trigger: ".hero",
-//        pin: true,
-//        end: `+=${innerHeight * 0.5}`,
-//        scrub: true,
-//        onUpdate: ({ scroll }) => {
-//          const isScrollingDown = scroll > lastScrollTop;
-//          document.querySelector('.hero-text span').classList.toggle('red', isScrollingDown);
-//          document.querySelector('.hero-text span').classList.toggle('black', !isScrollingDown);
-//          lastScrollTop = scroll;
-//        }
-//      }
-//    })
-//    tl.from('.hero-text', {
-//      scale: 5,   
-//      duration: 2,
-//    })
-//    .to('.hero-bg', {
-//      opacity: 1, 
-//      duration: 1.5,
-//      x:0
-//    },"1"); 
-//  }
-  
-  
+    tl.to(
+      ".hero-text-p",
+      {
+        opacity: 1,
+        ease: "power1.in",
+        y: 0,
+        duration : 1.5,
+        delay: 0.5,
+        scrub : true,
+        onComplete: () => {
+          spanToBold.forEach(span => {
+            span.classList.add("mini-text");
+            console.log("Span classes are added")
+          })
+        },
+      },
+      "2"
+    );
+  }
 
   _createLenis() {
     this.lenis = new Lenis({
       lerp: 0,
-      easing: function easeOutQuad(x){
+      easing: function easeOutQuad(x) {
         return 1 - (1 - x) * (1 - x);
       },
-    })
+    });
   }
 
   _render(time) {
     this.lenis.raf(time);
     requestAnimationFrame(this._render.bind(this));
   }
-
 }
-
 
 new App();

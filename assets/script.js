c3d05da60a6134da9cf8ce3a1cc579042b37900c
morty-9 <hzawnineseven@gmail.com>
2024-09-19 16:55:10 +0630

    new

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
 class App {
+
   constructor() {
-    const { innerheight } = window;
-    this.innerHeight = innerheight;
+    const { innerHeight } = window;
+    this.texts = [...document.querySelectorAll(".sec-one-text-effect p")];
+    this.innerHeight = innerHeight;
     this._initialize();
     this._render();
   }
@@ -10,80 +12,138 @@ class App {
     this._setInitialStates();
     this._createLenis();
     this._createHeroTextAnimation();
+    this._splitText();
   }
 
   _setInitialStates() {
-    gsap.set(".hero-text-p", {
-      opacity: 0,
-      y: 20,
-    });
+    gsap.set(".hero-text-p ",
+      {
+        opacity: 0,
+        y: 40,
+      });
   }
+  
+  _splitText() {
+    const split = new SplitType(".mini-text", { types: "words , chars" });
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".sec-one-text-effect",
+        start: "top 50",
+        end: "+=500",
+        scrub: true,
+        markers: true,
+        pin : true
+      }
+    });
+    tl.fromTo(".sec-one-text-effect h2",
+      {
+        opacity: 0,
+        y: 10,
+      }, {
+      opacity: 1,
+      y: 0,
+      ease: "power2.out",
+      duration: 3,
+    }
+      , "0");
+    
+      this.texts.forEach(text => {
+        tl.fromTo(text, {
+          y: 10,
+          duration: 2,
+          stagger : 3
+        }, {
+          y: 0,
+          opacity : 1
+        }
+        )
+      },)
+    tl.to(split.chars,
+      {
+        y: -1,
+        duration: 1.5,
+        color: "red",
+        stagger: 3,
+        fontWeight: "600",
+        delay: 4
+      },
+      "1"
+    );
+   
+  }
+    //texts.forEach(text => {
+    //  tl.
+    //})
+   
+    /*.fromTo(".section-one .two", {
+      
+      opacity: 0,
+        y: 20,
+      
+    }, {
+      opacity: 1,
+      y: 0,
+    },"1")*/
+
+//    
+  
 
   _createHeroTextAnimation() {
-    
     const tl = gsap.timeline({
       scrollTrigger: {
         trigger: ".hero",
-        pin: true,
-        end: `+=${innerHeight * 1.5}`,
-        scrub: true,
+        pin: true, 
+        end: `+=${this.innerHeight * 1.5}`, 
+        scrub: true, 
         markers: true,
-      
-        
       },
     });
-    const spanToBold = document.querySelectorAll(".hero-text-p span");
+
     tl.fromTo(
-      ".hero-text",
+      ".hero-text", 
       {
         scale: 2,
         x: -400,
-        duration: 3,
-        ease: "power3.in",
         color: "white",
         "-webkit-text-stroke-width": "1px",
         "-webkit-text-stroke-color": "black",
       },
       {
-        color: "black",
         scale: 1,
         x: 0,
+        color: "black",
       }
     );
-
+ 
     tl.to(
       ".hero-text-p",
       {
         opacity: 1,
         ease: "power1.in",
         y: 0,
-        duration : 1.5,
+        duration: 1.5,
         delay: 0.5,
-        scrub : true,
-        onComplete: () => {
-          spanToBold.forEach(span => {
-            span.classList.add("mini-text");
-            console.log("Span classes are added")
-          })
-        },
       },
-      "2"
+      "2" 
     );
   }
 
   _createLenis() {
     this.lenis = new Lenis({
-      lerp: 0,
+      lerp: 0.1, 
       easing: function easeOutQuad(x) {
         return 1 - (1 - x) * (1 - x);
       },
     });
+
+    gsap.ticker.lagSmoothing(0);
   }
 
-  _render(time) {
-    this.lenis.raf(time);
-    requestAnimationFrame(this._render.bind(this));
+  _render(time)
+  {
+    this.lenis.raf(time); 
+    requestAnimationFrame(this._render.bind(this)); 
   }
-}
+};
 
-new App();
+  document.addEventListener("DOMContentLoaded", () => { new App() });
